dc56d16f8262520021ebfb7637b792bd7fbb6869
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-04-07 07:54:03 -0700

    final task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,8 @@
  */
 int main(void)
 {
-	system("echo and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fputs(msg, stdout);
 	return (1);
 }
