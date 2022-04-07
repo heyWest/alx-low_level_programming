8cedc1e1c5be9871c48e21602b465538a4e4e0d4
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-04-07 07:55:53 -0700

    final task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	fputs(msg, stdout);
 	return (1);
