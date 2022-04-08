7a0136216b419914040f71b205fcc16b5ec15c5d
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-04-08 12:02:21 -0700

    final task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int m = 48;
+
+	int o = 48;
+
+	int n = 48;
+
+	int i;
+	int j = 48;
+
+	int k = 48;
+
+	for (i = 48; i <= 57; i++)
+	{
+		m = i;
+		for (j = n; j < 57; j++)
+		{
+			o = j + 1;
+			for (k = m; k <= 57; k++)
+			{
+				for (l = o; l <= 57; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (i == 57 && j == 56 && k == 57 && l == 57)
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
