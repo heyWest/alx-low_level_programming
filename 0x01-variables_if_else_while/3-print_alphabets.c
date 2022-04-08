81d209d0dd9b93a575233c9a85ecf3468680ea17
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-04-08 08:00:55 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,16 +3,21 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (0)
  */
 int main(void)
 {
 	char c = 'a';
 
+	char b = 'A';
+
 	while (c <= 'z')
 	{
 		putchar(c);
-		c++;
+	}
+	while (b <= 'Z')
+	{
+		putchar(b);
 	}
 	putchar('\n');
 	return (0);
