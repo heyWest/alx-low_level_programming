db78f509f574d3907a1cda27982b256866e800e1
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-04-08 10:10:32 -0700

    task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int j;
+	int k;
+	int i;
+	int l = 50;
+
+	int m = 49;
+
+	int n = 50;
+
+	for (i = 48; i < 56; i++)
+	{
+		n = l;
+		for (j = m; j < 57; j++)
+		{
+			k = n;
+			while (k <= 57)
+			{
+				putchar(i);
+				putchar(j);
+				puthcar(k);
+				putchar(',');
+				putchar(' ');
+				k++;
+			}
+			n++;
+		}
+		m++;
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
