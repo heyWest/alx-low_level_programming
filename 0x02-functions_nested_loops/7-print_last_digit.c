e73d1b123a043e037e04fb1e4155db395173d246
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-04-12 16:50:09 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: takes in an integer value
+ * Return: Always 0;
+ */
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		_putchar(-n % 10 + '0');
+	}
+	else
+	{
+		_putchar(n % 10 + '0');
+	}
+}
