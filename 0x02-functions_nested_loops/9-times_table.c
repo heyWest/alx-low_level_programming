eb7d0e053c7b1e2f6e8f90c207ccfcdb8fc456de
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-04-13 04:54:26 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 times table
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+			if ((k / 10) == 0)
+			{
+				if (j == 0)
+				{
+					_putchar('0');
+				}
+				if (j != 0)
+				{
+					_putchar(' ');
+					_putchar((k % 10) + '0');
+				}
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
+
