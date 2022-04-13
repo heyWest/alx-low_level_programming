aa831ae236a5c975d896931dd69d3b8373c5f019
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-04-12 17:13:46 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute of the day
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 48; i < 51; i++)
+	{
+		for (j = 48; j < 52; j++)
+		{
+			for (k = 48; k < 54; k++)
+			{
+				for (l = 48; l < 58; l++)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
