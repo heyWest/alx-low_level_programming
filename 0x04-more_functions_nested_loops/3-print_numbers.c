caf4175a1da7893121589560e7c9af179653aa7f
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-04-14 05:31:43 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, 0 t0 9
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int c = 48;
+
+	while (c < 58)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
