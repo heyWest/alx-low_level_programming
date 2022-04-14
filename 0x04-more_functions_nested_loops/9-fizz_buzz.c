47f3c57ec81d88548b1fcf4d62478ffc7315b427
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-04-14 06:11:50 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return Always 0.
+ */
+int main(void)
+{
+	int m = 1;
+
+	while (m < 100)
+	{
+		if ((m % 3 == 0) && (m % 5 != 0))
+			printf("Fizz ");
+		else if ((m % 5 == 0) && (m % 3 != 0))
+			printf("Buzz ");
+		else if ((m % 5 == 0) && (m % 3 == 0))
+			printf("FizzBuzz");
+		else
+			printf("%d ", m);
+		m++;
+	}
+	printf("Buzz");
+}
