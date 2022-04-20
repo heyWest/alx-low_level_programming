befbef2f9359503bc9346334311ebdf8b07d7c3e
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-04-20 00:09:28 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer as a parameter and updates value to 98
+ * @n: pointer
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
