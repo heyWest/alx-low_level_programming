6090267b9e8277c30cb21cff61551ad61c067c11
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-04-20 04:33:36 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: first parameter
+ * @src: second parameter
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
