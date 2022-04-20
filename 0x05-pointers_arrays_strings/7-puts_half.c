1792af30fd40527aa73ad748df5d5c1a05e8c66c
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-04-20 03:47:56 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,7 +17,7 @@ void puts_half(char *str)
 	}
 	if (i % 2 == 0)
 	{
-		j = (i / 2) - 1;
+		j = i / 2;
 		while (j < i)
 		{
 			_putchar(str[j]);
@@ -26,7 +26,7 @@ void puts_half(char *str)
 	}
 	else
 	{
-		j = ((i - 1) / 2) - 1;
+		j = (i - 1) / 2;
 		while (j < i)
 		{
 			_putchar(str[j]);
