ebd9c8b37e6903784f36bd123aaa841eabcc134f
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-04-20 03:45:37 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: takes in a string pointer as a parameter
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+
+	int j;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 0)
+	{
+		j = (i / 2) - 1;
+		while (j < i)
+		{
+			_putchar(str[j]);
+			j++;
+		}
+	}
+	else
+	{
+		j = ((i - 1) / 2) - 1;
+		while (j < i)
+		{
+			_putchar(str[j]);
+			j++;
+		}
+	}
+	_putchar('\n');
+}
