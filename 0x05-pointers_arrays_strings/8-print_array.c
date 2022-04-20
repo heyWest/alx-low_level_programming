4b233bcc7aa80c962637ba9f4075fd5a2a3ba834
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-04-20 03:58:30 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array
+ * @a: first parameter
+ * @n: second parameter
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		_putchar(a[i]);
+		if (i == n - 1)
+			break;
+		_putchar(',');
+		_putchar(' ');
+		i++;
+	}
+	_putchar('\n');
+}
