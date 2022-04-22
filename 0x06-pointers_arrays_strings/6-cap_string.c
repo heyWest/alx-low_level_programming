837f53cbf8f2031dba8a8fc47b6d430964529dde
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-04-22 02:42:30 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to be capitalized
+ * Return: capitalized string
+ */
+char *cap_string(char *s)
+{
+	int i = 1;
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= 32;
+
+	while (s[i] != '\0')
+	{
+		if (s[i - 1] == " ")
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+				s[i] -= 32;
+		}
+		i++;
+	}
+}
