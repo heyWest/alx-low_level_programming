3453e918caa834e25b6cd791535543f07bf5fa71
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-04-22 01:50:33 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: pointer to array
+ * @n: number of array elements
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int arr[n];
+
+	int i = n - 1;
+
+	int k = 0;
+
+	int m;
+
+	while (i >= 0 && k < n)
+	{
+		arr[k] = a[i];
+		i--;
+		k++;
+	}
+	for (m = 0; m < n; m++)
+	{
+		a[m] = arr[m];
+	}
+}
