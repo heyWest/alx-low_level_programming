76c644a6ad3c9f12393d7934e4977c324d86d779
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-04-22 00:08:48 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: holds first string
+ * @src: holds second string
+ * Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a = 0;
+
+	int b = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	while (src[b] != '\0')
+	{
+		dest[a] = src[j];
+		b++;
+		a++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
