1e08e8e9e655b6f9caff134342fb7413548e54e8
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-04-26 07:57:34 -0700

    task 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,7 +9,7 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j;
+	int i;
 
 	int acc1 = 0;
 
@@ -17,17 +17,8 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
-		{
-			if (i == j)
-			{
-				acc1 += a[i][j];
-			}
-			else if (j + i == size - 1)
-			{
-				acc2 += a[i][j];
-			}
-		}
+		acc1 += *(a + i * size + i);
+		acc2 += *(a + i * size + size - i - 1);
 	}
-	printf("%i", "%i\n", acc1, acc2);
+	printf("%i, %i\n", acc1, acc2);
 }
