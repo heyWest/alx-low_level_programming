2dca4a7a1772c0c369f08cfb17d93f6cde685270
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-04-28 05:14:41 -0700

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer value
+ * @y: base
+ * Return: returns square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	if (
