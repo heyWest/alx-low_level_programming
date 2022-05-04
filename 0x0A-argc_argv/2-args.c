092862f06c35a8e334e166ed03953fddbaf3b0f2
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-05-04 06:30:36 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main."
+#include <stdio.h>
+
+/**
+ * main - Prints all arguments
+ * @argc: size of array
+ * @argv: array of arguments
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int count = 0;
+
+	while (count < argc)
+	{
+		printf("%s\n", argv[count]);
+		count++;
+	}
+	return (0);
+}
