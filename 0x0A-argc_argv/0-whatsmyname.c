31a20a15ced17cfeb516393e1d9d0f2ce50a614c
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-05-04 05:53:19 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print program name
+ * @argc - size of argv
+ * @argv - array of arguments
+ * Return : Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
