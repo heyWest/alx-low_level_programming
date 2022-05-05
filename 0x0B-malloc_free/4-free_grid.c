14c7b23aa388176511ec47330113ad98378cc117
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-05-05 07:41:35 -0700

    task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * free_grid - frees a 2D grid
+ * @grid: width of grid
+ * @height: height of grid
+ * Return: free grid
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
