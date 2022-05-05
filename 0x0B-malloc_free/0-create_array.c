5de32ab8ddcd976bc3fdc611d44f331955118c36
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-05-05 06:08:25 -0700

    first push

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * create_array - create array with malloc
+ * @size: size of array
+ * @c: character argument
+ * Return: pointer to array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+
+	int i;
+
+	s = (int *) malloc((size + 1) * sizeof(char));
+	if (size == 0 || s == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+thi is a non empty readme
