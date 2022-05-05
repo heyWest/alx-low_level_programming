085eac59230338fdc5f6b674af4a4c01737ed9a7
Daniel Manasseh Ofei Kwafo <79490695+heyWest@users.noreply.github.com>
2022-05-05 15:46:33 +0000

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,5 +1,51 @@
 #include "main.h"
 
+int isSpace(char c);
+int startIndex(char *s, int index);
+int endIndex(char *s, int index);
+int countWords(char *s);
+/**
+ * strtow - splits a string into words
+ * @str: string of words to be split
+ * Return: double pointer to strings
+ */
+
+char **strtow(char *str)
+{
+	char **ptr;
+	int i, k, len, start, end, j = 0;
+	int words =  countWords(str);
+
+	if (!str || !countWords(str))
+		return (NULL);
+	ptr = malloc(sizeof(char *) * (words + 1));
+	if (!ptr)
+		return (NULL);
+	for (i = 0; i < words; i++)
+	{
+		start = startIndex(str, j);
+		end = endIndex(str, start);
+		len = end - start;
+		ptr[i] = malloc(sizeof(char) * (len + 1));
+		if (!ptr[i])
+		{
+			i -= 1;
+			while (i >= 0)
+			{
+				free(ptr[i]);
+					i--;
+			}
+			free(ptr);
+			return (NULL);
+		}
+		for (k = 0; k < len; k++)
+			ptr[i][k] = str[start++];
+		ptr[i][k++] = '\0';
+		j = end + 1;
+	}
+	ptr[i] = NULL;
+	return (ptr);
+}
 /**
  * isSpace - determines if character is a space or not
  * @c: input char
@@ -59,46 +105,4 @@ int countWords(char *s)
 		s++;
 	}
 	return (words);
-}
-
-/**
- * strtow - splits a string into words
- * @str: string of words to be split
- * Return: double pointer to strings
- */
-char **strtow(char *str)
-{
-	char **ptr;
-	int i, k, len, start, end, j = 0;
-	int words =  countWords(str);
-
-	if (!str || !countWords(str))
-		return (NULL);
-	ptr = malloc(sizeof(char *) * (words + 1));
-	if (!ptr)
-		return (NULL);
-	for (i = 0; i < words; i++)
-	{
-		start = startIndex(str, j);
-		end = endIndex(str, start);
-		len = end - start;
-		ptr[i] = malloc(sizeof(char) * (len + 1));
-		if (!ptr[i])
-		{
-			i -= 1;
-			while (i >= 0)
-			{
-				free(ptr[i]);
-					i--;
-			}
-			free(ptr);
-			return (NULL);
-		}
-		for (k = 0; k < len; k++)
-			ptr[i][k] = str[start++];
-		ptr[i][k++] = '\0';
-		j = end + 1;
-	}
-	ptr[i] = NULL;
-	return (ptr);
 }
