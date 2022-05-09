da952e26c5962b2e7458d3651ed9821430db4eff
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-05-09 06:31:06 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * malloc_checked - a function that allocates memory using malloc
+ * @b: memory number
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
