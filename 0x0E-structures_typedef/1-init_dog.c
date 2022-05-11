e2fd09bfd92c8f37a92af3967e1cfe3d79cb5935
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-05-11 03:28:45 -0700

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of type struct
+ * @d: array
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: dog structure
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
