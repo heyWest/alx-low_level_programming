819853f92aa5cd94b02eb88cc6e7740ebab93556
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-05-13 03:58:14 -0700

    task 3 main

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+
+/**
+ * op_add - performs addition
+ * @a: first integer
+ * @b: second integer
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - performs subtraction
+ * @a: first integer
+ * @b: second integer
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - performs multiplication
+ * @a: first integer
+ * @b: second integer
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - performs division
+ * @a: dividend
+ * @b: divisor
+ * Return: quotient
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - perform modular arithmetic
+ * @a: first integer
+ * @b: second integer
+ * Return: a mod b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
