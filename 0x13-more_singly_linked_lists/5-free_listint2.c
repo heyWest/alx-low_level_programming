738f6ed0221d43b09ddd10d247ff97c1f9b88165
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-05-26 13:35:09 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a list
+ * @head: head node
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t = *acc;
+
+	if (head == NULL)
+		return;
+	while (*head)
+	{
+		acc = (*head)->next;
+		free(*head);
+		*head = acc;
+	}
+	*head = NULL;
+}
