65a55ed5e92b34dcbf60b3cd70fbc249ded358d9
Daniel Manasseh Ofei Kwafo <heyitzwest@gmail.com>
2022-05-26 02:43:18 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_listint - a function that prints all the elements of a listint_t list
+ * @h: list
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+
+	n = 0;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
